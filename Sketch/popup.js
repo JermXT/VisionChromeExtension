d8186ac3f80329dfd448c255af419d3f0206df5b
Dana Feng <danafeng308@gmail.com>
2020-07-28 10:13:49 -0700

    changed timer

--- Sketch/popup.js
+++ Sketch/popup.js
@@ -1,3 +1,4 @@
+
 let slider1,
   slider2,
   studyTime,
@@ -14,24 +15,61 @@ let slider1,
   seconds,
   bMinutes,
   bSeconds,
-  exit;
+  exit, start;
+  function createSlider1() {
+    slider1 = createSlider(0, 90, 100);
+    slider1.position(width * 0.45, 150);
+    slider1.style("width", "80px");
+    slider1.size(width / 2);
+  };
+  
+  function createSlider2() {
+    slider2 = createSlider(0, 10, 100);
+    slider2.position(width * 0.45, 250);
+    slider2.style("width", "80px");
+    slider2.size(width / 2);
+  };
+  function mouseClicked() {
+    //click start studying button, starts with studying
+    if (
+      mouseX > 200 &&
+      mouseX < 300 &&
+      mouseY > 350 &&
+      mouseY < 390 &&
+      settings == false
+    ) {
+      settings = true;
+      studyTimerMode = true;
+      console.log("variables to store sent to background");
+      messageBackground({"timerLength": studyTime, "breakLength": breakTime, "reset":true,"timerOn": true })
+    }
+    //Click exit button
+    else if (
+      mouseX > 200 &&
+      mouseX < 300 &&
+      mouseY > 350 &&
+      mouseY < 390 &&
+      settings == true
+    ) {
+      settings = false;
+      studyTimerMode = false;
+      messageBackground({"timerLength": studyTime, "breakLength": breakTime, "reset":false,"timerOn":false})
+    }
+  };
+
 function setup(){
+    start = false;
     createCanvas(500, 500);
     colorMode(HSB, 360, 100, 100);
-    createSlider1();
-    createSlider2();
     settings = false;
     studyTimerMode = false;
-    setInterval(timeIt, 1000);
-    messageBackground({"getData":true})
-    //message to background
-    //Testing code
-    //messageBackground({"timerLength": 5 , "breakLength": 5, "reset":true});
-    //messageBackground({"testing":true})
+    createSlider1();
+    createSlider2();
 }
 function draw() {
     background(210, 20, 95);
-  
+    
+    //Creates landing page for popup and start button with sliders
     if (settings == false) {
       textAlign(LEFT);
       text(
@@ -47,198 +85,69 @@ function draw() {
       textSize(14);
       text(`Take a break for ${breakTime} minutes`, 20, 250);
   
-      studyTimeSec = studyTime * 60;
-      breakTimeSec = breakTime * 60;
+      studyTimeSec = studyTime * 60; //in seconds
+      breakTimeSec = breakTime * 60; //in seconds
       makeButton();
+      //messageBackground({"getData":true, "timerLength": studyTime, "breakLength": breakTime});
     }
     
     if (settings == false) {
       slider1.show();
       slider2.show();
     }
+    
     if (settings == true) {
       slider1.hide();
       slider2.hide();
-    }
-  
-    if (settings == true) {
       exitButton();
+      
+    }
+    if(settings == true && start == true){
+      textSize(20);
+      textAlign(CENTER);
+      //text("Rest your eyes, stand up, and move around until the snow fills the screen.",0, 30, width );
+      text(`Screen/Study Time Left: ${currentHours} hours, ${currentMinutes} min, ${currentSeconds} sec`, 0, height/4, width);
     }
   
-    if (settings == true && studyTimerMode == true) {
-      if (hours >= 1) {
-        textAlign(CENTER);
-        if (minutes == 1) {
-          text(
-            `You have ${hours} HOUR, ${minutes} MINUTE, and ${seconds} SECONDS until your break!`,
-            width / 2,
-            height / 2
-          );
-        } else {
-          text(
-            `You have ${hours} HOUR, ${minutes} MINUTES, and ${seconds} SECONDS until your break!`,
-            width / 2,
-            height / 2
-          );
-        }
-      } else if (hours == 0 && minutes > 1) {
-        textAlign(CENTER);
-        text(
-          `You have ${minutes} MINUTES and ${seconds} SECONDS until your break!`,
-          width / 2,
-          height / 2
-        );
-      } else if (hours == 0 && minutes == 1) {
-        textAlign(CENTER);
-        text(
-          `You have ${minutes} MINUTE and ${seconds} SECONDS until your break!`,
-          width / 2,
-          height / 2
-        );
-      } else if (minutes == 0) {
-        textAlign(CENTER);
-        text(
-          `You have ${seconds} SECONDS until your break!`,
-          width / 2,
-          height / 2
-        );
-      }
-    }
-  
-    if (settings == true && studyTimerMode == false) {
-      if (bMinutes > 1) {
-        textAlign(CENTER);
-        text(
-          `You have ${bMinutes} MINUTES and ${bSeconds} SECONDS until going back to the screen!`,
-          width / 2,
-          height / 2
-        );
-        text("Rest your eyes, stand up, and move around until the snow fills the screen.",0, height/2 + 30, width );
-      } else if (bMinutes == 1) {
-        textAlign(CENTER);
-        text(
-          `You have ${bMinutes} MINUTE and ${bSeconds} SECONDS until going back to the screen!`,
-          width / 2,
-          height / 2
-        );
-        text("Rest your eyes, stand up, and move around until the snow fills the screen.",0, height/2 + 30, width );
-      } else if (bMinutes == 0) {
-        textAlign(CENTER);
-        text(
-          `You have ${bSeconds} SECONDS until going back to the screen!`,
-          width / 2,
-          height / 2
-        );
-        text("Rest your eyes, stand up, and move around until the snow fills the screen.",0, height/2 + 30, width );
-      }
-    }
-  }
+ 
   
   function makeButton() {
+    textSize(15);
+    //textAlign(CENTER);
     button = rect(200, 350, 100, 40);
     text("START!", 225, 374);
   }
   
   function exitButton() {
+    textSize(15);
+    textAlign(LEFT);
     exit = rect(200, 350, 100, 40);
-    text("EXIT", 250, 374);
+    text("EXIT!", 225, 374);
   }
   
-  function createSlider1() {
-    slider1 = createSlider(0, 90, 100);
-    slider1.position(width * 0.45, 150);
-    slider1.style("width", "80px");
-    slider1.size(width / 2);
-  }
   
-  function createSlider2() {
-    slider2 = createSlider(0, 10, 100);
-    slider2.position(width * 0.45, 250);
-    slider2.style("width", "80px");
-    slider2.size(width / 2);
-  }
   
-  function mouseClicked() {
-    //start studying button
-    if (
-      mouseX > 200 &&
-      mouseX < 300 &&
-      mouseY > 350 &&
-      mouseY < 390 &&
-      settings == false
-    ) {
-      settings = true;
-      studyTimerMode = true;
-      messageBackground({"getData":false,"timerLength": studyTime, "breakLength": breakTime, "reset":true,"timerOn": true })
-    }
-    //Exit button
-    else if (
-      mouseX > 200 &&
-      mouseX < 300 &&
-      mouseY > 350 &&
-      mouseY < 390 &&
-      settings == true
-    ) {
-      settings = false;
-      studyTimerMode = false;
-      messageBackground({"getData":false,"timerLength": studyTime, "breakLength": breakTime, "reset":false,"timerOn":false})
-    }
-  }
   
-  function timeIt() {
-    if (studyTimeSec > 0) {
-      studyTimeSec--;
-    }
-    if (studyTimeSec == 0) {
-      studyTimeSec = studyTime * 60;
-      studyTimerMode = true;
-    }
   
-    if (studyTimerMode == false) {
-      if (breakTimeSec > 0) {
-        breakTimeSec--;
-      }
-      if (breakTimeSec == 0) {
-        studyTimerMode = false;
-        breakTimeSec = breakTime * 60;
-      }
-    }
-    hours = floor(studyTimeSec / 3600);
-    minutes = floor(studyTimeSec / 60) - hours * 60;
-    seconds = studyTimeSec % 60;
-  
-    bMinutes = floor(breakTimeSec / 60);
-    bSeconds = breakTimeSec % 60;
-  }
   
 
+}
 
-//message should be{"timeleft":timeLeft,"timerOn":true/false, "breaktime":true/false}
-//times should be in minutes
-//all other time are in seconds
-//this function should be called every time the popup is clicked(since that "refreshes" the popup)
-//then while the popup is open it should decrement the timer on its own
 function messageBackground(message){
-    chrome.runtime.sendMessage(message, function(response) {
+  console.log("from popup: sent the variables");
+  chrome.runtime.sendMessage(message, function(response) {
+        
         console.log(response);
-        if(response.timerOn == false){
-            //makes the popup display settimer screen
-            settings = false;
-        }else if(response.timerOn == true){
-            settings = true;
-            if(response.breakTime == true){
-                //makes the popup display time left screen, with the words time left till studying
-                studyTimerMode = true;
-                breakTimeSec = response.timeleft
-            }else{
-                //makes the popup display time left screen, with the words time left till break
-                studyTimerMode = false;
-                studyTimeSec = response.timeleft
-            }
-        }
-        response.timerLength
-        //response will be time left in seconds : {timeleft: seconds}
 
       });
 }
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    currentHours = request.hours;
+    currentMinutes = request.minutes;
+    currentSeconds = request.seconds;
+    start = request.startNow;
+  }
+
+);
 
--- background.js
+++ background.js
@@ -1,47 +1,83 @@
 
 console.log('background running');
-let date = new Date;
 
-console.log(date.getHours());
+
 let timerLength, breakLength, startTime, timerOn = false, startSnowAnimation, timedSnowAnimation, breakTime = false, timeLeft, ticker, tickerFunction
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        //testing code
-        if (request.testing == true) {
-            snowAnimation();
-            sendResponse({ "timeleft": "hi" })
-
-        }
-        //
-        if (request.getData == true) {
-            // update timer
-            if (timerOn) {
-                //let timeElapsed = (date.getDay() * 24 * 60 * 60 + date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()) - startTime;
-                
-                //console.log(date.getDay() * 24 * 60 * 60 + date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds())
-
-                timeLeft = timerLength * 60 - ticker
-                console.log("timeLeft: "+ timeLeft)
-                sendResponse({ "timeleft": timeLeft, "timerOn": true, "breaktime": breakTime })
-
-            } else {
-                sendResponse({ "timeleft": timeLeft, "timerOn": false, "breaktime": false })
-            }
+         if (request.reset == true) {
+                //everything is in milliseconds
+                //start timer used https://www.w3schools.com/howto/howto_js_countdown.asp
+                console.log("from background: received variables and now logging them");
+                timerLength = request.timerLength * 60 * 1000
+                breakLength = request.breakLength * 60 * 1000
+                console.log("now logging timerLength");
+                console.log(timerLength);
+                let startTime= new Date().getTime();
+                let countdown = setInterval(function(){
+                    let now = new Date().getTime();
+                    let distance = timerLength + ((startTime - now));
+                    console.log(distance);
+                    // if (distance === 0){
+                    //     switchStartTime();
+                    //     console.log("restarted ")
+                    // }
+                    let hours = Math.floor(((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+                    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    
+                    //console.log(hours + "h" + minutes + 'm' + seconds + "s");
+                    messagePopUp({'hours': hours, 'minutes': minutes, 'seconds': seconds, "startNow": true});
+                    function messagePopUp(message){
+                        console.log("from background: sent countdown to popup");
+                        chrome.runtime.sendMessage(message, function(response) {
             
-            
-        } else {
-            if (request.reset == true) {
-                //start timer
-                timerLength = request.timerLength
-                breakLength = request.breakLength
-                ticker = 0
-                tickerFunction = setInterval(function(){ticker +=1}, 1000)
-                //startTime = date.getDay() * 24 * 60 * 60 + date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()
+                              console.log(response);
+                              // if(response.timerOn == false){
+                              //     //makes the popup display settimer screen
+                              //     settings = false;
+                              // }else if(response.timerOn == true){
+                              //     settings = true;
+                              //     if(response.breakTime == true){
+                              //         //makes the popup display time left screen, with the words time left till studying
+                              //         studyTimerMode = true;
+                              //         breakTimeSec = response.timeleft
+                              //     }else{
+                              //         //makes the popup display time left screen, with the words time left till break
+                              //         studyTimerMode = false;
+                              //         studyTimeSec = response.timeleft
+                              //     }
+                              // }
+                              //console.log(response.timeLeft);
+                              //response.timerLength
+                              //response will be time left in seconds : {timeleft: seconds}
+                      
+                            });
+                    //document.getElementById("timerShow").innerHTML = hours + "h" + minutes + 'm' + seconds + "s";
+                        }
+                    
+                    
+                    // if (distance < 0) {
+                    //     clearInterval(x);
+                    //     document.getElementById("demo").innerHTML = "EXPIRED";
+                    //   }
                 
-                timerOn = true
-                startSnowAnimation = setTimeout(snowAnimation, timerLength*1000*60)
-                sendResponse({ "timeleft": timerLength, "timerOn": true, "breaktime": false})
+                }, 1000);
+                //snowAnimation();
+                // while (startTime - new Date().getTime() !== timerLength){
+                //     sendResponse();
+                //     console.log("Sent");
+                //     sendResponse({ "timeleft": timerLength - startTime - new Date().getTime(), "timerOn": true, "breaktime": false});
+                // }
+                
+                // ticker = 0
+                // tickerFunction = setInterval(function(){ticker +=1}, 1000)
+                // //startTime = date.getDay() * 24 * 60 * 60 + date.getHours() * 60 * 60 + date.getMinutes() * 60 + date.getSeconds()
+                
+                // timerOn = true
+                // startSnowAnimation = setTimeout(snowAnimation, timerLength*1000*60)
+                
             }else if(request.timerOn == false){
                 //shut down timer
                 timerOn = false
@@ -62,7 +98,7 @@ chrome.runtime.onMessage.addListener(
             } else {
                 sendResponse({ "timeleft": timeLeft, "timerOn": false, "breaktime": false })
             }*/
-        }
+        
     });
 function snowAnimation() {
     timedSnowAnimation = setTimeout(stopAnimation, breakLength*1000*60);
@@ -75,6 +111,9 @@ function stopAnimation() {
     startSnowAnimation = setTimeout(snowAnimation, timerLength * 1000 * 60)
     breakTime = false
 
+}
+function switchStartTime(){
+    startTime = new Date().getTime();
 }
 
 //{animation:true/false}
